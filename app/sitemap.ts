3e666f80700f1c30e6c977c4e6bdfe5dc3051388
Seung Min <parksm1415@gmail.com>
2026-01-15 21:04:13 +0900

    sitemap 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://hang-on.vercel.app",
+      url: "https://hangon-bay.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
